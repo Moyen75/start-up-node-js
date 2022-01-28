6cad9f6dc0934fbbe141b3ed0e0c7611fe569d79
moyen90 <moyen@connekt.studio>
2022-01-28 15:10:24 +0600

    start up node js

---
+++ middleware/authenticate.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+const root = require("app-root-path");
+const config = require(`${root}/config`);
+
+module.exports = {
+  authRoute: async (req, res, next) => {
+    if (req.headers.authorization.starsWith('Bearer ')) {
+      const token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, config.session.jwt_secret);
+      const firebase_user = await admin.auth().getUser(decoded.uid);
+      if (decoded.email != firebase_user.email || firebase_user.disabled) {
+        return res.status(403).json({ error: "Not authorized " });
+      }
+      try {
+        req.fb_user_id = decoded.uid;
+        req.auth_user_id = req.session.current_user_id;
+        req.auth_org_id = req.session.current_org_id;
+        req.auth_role = req.session.current_user_role;
+        req.auth_session_id = req.session.current_session_id;
+        next();
+      } catch (err) {
+        console.log(err);
+        return res.status(403).send("Unauthorized");
+      }
+    } else {
+      return res.status(403).send("Unauthorized");
+    }
+  },
+};
+
+module.exports = {};
