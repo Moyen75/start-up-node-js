dc0a1b2fa895c4fa35dcb3524d6502ea878d9156
moyen90 <moyen@connekt.studio>
2022-02-01 10:58:55 +0600

    news added

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,11 +3,7 @@ const root = require("app-root-path");
 // const config = require(`${root}/config`);
 var admin = require("firebase-admin");
 
-var serviceAccount = require(`${root}/firebase-adminsdk.json`);
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
 
 admin.initializeApp({
   credential: admin.credential.cert({
@@ -16,20 +12,17 @@ admin.initializeApp({
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') // NOW THIS WORKS!!!
   })
 })
-console.log(process.env.FIREBASE_PRIVATE_KEY)
 
 const authRoute = async (req, res, next) => {
   if (req.headers?.authorization) {
     const token = req.headers.authorization.split(" ")[1];
 
-
     // verify user token
     const decodedUser = await admin.auth().verifyIdToken(token)
     console.log(decodedUser)
-    // const firebase_user = await admin.auth().getUser(decoded.uid);
-    // if (decoded.email != firebase_user.email || firebase_user.disabled) {
-    //   return res.status(403).json({ error: "Not authorized " });
-    // }
+    if (!decodedUser.email) {
+      return res.status(403).json({ error: "Not authorized " });
+    }
     try {
       // req.fb_user_id = decodedUser.uid;
       // req.auth_user_id = req.session.current_user_id;
